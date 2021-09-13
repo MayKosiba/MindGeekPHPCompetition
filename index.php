0f3025f20b9bd15ddbf149c60a4c1378618bf7aa
May Kosiba <maykosiba@gmail.com>
2021-09-12 22:32:58 -0400

    project setup

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-$m = new Mustache_Engine;
-echo $m->render('Hello, {{planet}}!', array('planet' => 'World')); // "Hello, World!"
+require 'vendor/mustache/mustache/src/Mustache/Autoloader.php';
+require 'lang/strings.php';
+Mustache_Autoloader::register();
+$m = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates')
+));
+echo $m->render($DIR['gameScreen']); // "Hello, World!"
